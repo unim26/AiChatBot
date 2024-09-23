f23dde2a576652932d0bb23f846f93e2f15fc8ad
Abhishek Kumar <93076602+Minuabhi92@users.noreply.github.com>
2024-09-23 09:10:12 +0530

    Update consts.dart

--- lib/consts.dart
+++ lib/consts.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-String geminiApiKey = "AIzaSyB7M1MhsZfJ2O7NM9q05phoddHPiz39Cvg";
+String geminiApiKey = "AIzaSyBxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
 TextEditingController messagecontroller = TextEditingController();
 TextEditingController suggestioncontroller = TextEditingController();
